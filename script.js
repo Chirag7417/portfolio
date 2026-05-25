50f49a60e7eceae551b1cb7f6735ad7d250f52c5
Your Name <your@email.com>
2026-05-25 18:07:50 +0530

    portfolio website

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-console.log("Portfolio Loaded");
+// ================= PAGE LOADED =================
+
+console.log("Chirag Portfolio Loaded Successfully");
+
+// ================= ACTIVE NAVBAR =================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop;
+
+    if (pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+
+  });
+
+});
+
+// ================= CONTACT FORM =================
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  alert("Message Sent Successfully!");
+
+  form.reset();
+
+});
+
+// ================= SCROLL ANIMATION =================
+
+const cards = document.querySelectorAll(
+  ".card, .skill-card, .project-card"
+);
+
+window.addEventListener("scroll", () => {
+
+  cards.forEach(card => {
+
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < window.innerHeight - 100){
+
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+
+    }
+
+  });
+
+});
+
+// ================= INITIAL STYLE =================
+
+cards.forEach(card => {
+
+  card.style.opacity = "0";
+  card.style.transform = "translateY(40px)";
+  card.style.transition = "0.6s";
+
+});
